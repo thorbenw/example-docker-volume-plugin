bab2e1e6b13db5b97d410d83589fbd68ccfe2fa1
Thorben Wolkersdorfer <twolkersdorfer@gmail.com>
2025-08-03 00:42:10 +0200

    Added map conversion and deletion functionality to `Options` struct in package `mount`.

--- src/mount/options.go
+++ src/mount/options.go
@@ -9,6 +9,18 @@ import (
 	"github.com/thorbenw/example-docker-volume-plugin/utils"
 )
 
+// region Package globals
+
+const (
+	DEFAULT_DELETION_MARK = "-"
+)
+
+var (
+	DeletionMark = DEFAULT_DELETION_MARK
+)
+
+// region Option struct
+
 type Option struct {
 	Key   string
 	Value string
@@ -49,6 +61,23 @@ func NewOptions(capacity int) Options {
 	return Options{options: &options}
 }
 
+func (o Options) SetMap(value *map[string]string) error {
+	if o.options == nil {
+		return errors.New("mount.Options.Set(): options must be initialized using NewOptions()")
+	}
+
+	if value == nil {
+		return errors.New("mount.Options.Set(): value must not be nil")
+	}
+
+	options := make([]string, 0, len(*value))
+	for k, v := range *value {
+		options = append(options, fmt.Sprintf("%s=%s", strings.TrimSpace(k), strings.TrimSpace(v)))
+	}
+
+	return o.Set(strings.Join(options, ","))
+}
+
 func (o Options) Set(value string) error {
 	if o.options == nil {
 		return errors.New("mount.Options.Set(): options must be initialized using NewOptions()")
@@ -69,10 +98,16 @@ func (o Options) Set(value string) error {
 			return option.Key == opt.Key
 		})
 
-		if idx >= 0 {
-			(*o.options)[idx] = opt
+		if opt.Value == DeletionMark {
+			if idx >= 0 {
+				*o.options = slices.Delete(*o.options, idx, idx+1)
+			}
 		} else {
-			*o.options = append(*o.options, opt)
+			if idx >= 0 {
+				(*o.options)[idx] = opt
+			} else {
+				*o.options = append(*o.options, opt)
+			}
 		}
 	}
 
@@ -94,4 +129,31 @@ func (o Options) String() string {
 	}
 
 	return strings.Join(options, ",")
+}
+
+func (o Options) Map() map[string]string {
+	var len_options int
+	if o.options != nil {
+		len_options = len(*o.options)
+	}
+	if len_options < 1 {
+		return map[string]string{}
+	}
+
+	options := make(map[string]string, len_options)
+	for _, option := range *o.options {
+		options[option.Key] = option.Value
+	}
+
+	return options
+}
+
+func (o Options) Len() int {
+	var result int
+
+	if o.options != nil {
+		result = len(*o.options)
+	}
+
+	return result
 }
